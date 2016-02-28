05c437ee42af1e91f93ca328d6bc569c2738d39b
Arisu Desu <arisu@arisu.me>
2016-02-28 11:47:26 +0600

    base texture impl

---
+++ include/texture.h
@@ -0,0 +1,32 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <vector>
+#include <glbinding/gl/gl.h>
+
+class Texture;
+
+class AbstractTextureLoader
+{
+public:
+    virtual Texture::TextureFormat GetFormat();
+    virtual int GetWidth() = 0;
+    virtual int GetHeight() = 0;
+    virtual const vector<unsigned char> GetData() = 0;
+};
+
+class Texture
+{
+    GLuint m_handle;
+
+public:
+    Texture(const AbstractTextureLoader& loader);
+    void Bind();
+
+    enum TextureFormat
+    {
+        RGBA = GL_RGBA,
+    };
+};
+
+#endif // TEXTURE_H
